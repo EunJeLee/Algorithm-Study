9998912e9e27f4f1ae71521649aeea061768036e
Eun_Jelly <dldmswp3@naver.com>
2022-04-20 01:03:30 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+  int main(){
+    int a;
+    scanf("%d",&a);
+    for(int i=a-1;i>=0;i--){
+      for(int j=0;j<a-i-1;j++)
+        printf(" ");
+      for(int j=0;j < i*2+1;++j)
+        printf("*");
+      printf("\n");
+    }
+    }
