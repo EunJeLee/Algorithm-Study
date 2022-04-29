62a9876ca011c8983047ce2213dc49634f5cf43e
Eun_Jelly <dldmswp3@naver.com>
2022-04-29 15:48:24 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <memory.h>
+#include <random>
+#include <unordered_map>
+#include <math.h>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+#define FASTIO() ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+#define pb push_back
+#define mp make_pair
+
+using namespace std;
+
+int n, m;
+int isused[9];
+int arr[9];
+int ans[9];
+
+void bktk(int k){
+  if(k == m){
+    for(int i = 0; i < m; i++){
+      cout << ans[i] << ' ';
+    }
+    cout << endl;
+    return;
+  }
+  int prev = -1;
+  for(int i = 1; i <= n; i++){
+    if(!isused[i] && ans[k-1] <= arr[i] && prev != arr[i]){
+      ans[k] = arr[i];
+      isused[i] = 1;
+      prev = arr[i];
+      bktk(k+1);
+      isused[i] = 0;
+    }
+  }
+}
+
+
+int main(){
+  FASTIO()
+  cin >> n >> m;
+  for(int i = 1; i <= n; i++) cin >> arr[i];
+  sort(arr+1, arr+n+1);
+  bktk(0);
+
+  return 0;
+}
