510b97f7594c94780b316e2c4e78806017b507fa
Eun_Jelly <dldmswp3@naver.com>
2022-08-05 10:57:02 +0900

    [Silver IV] Title: 상근이의 여행, Time: 80 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+
+
+void solve(){
+    int N, M;
+    cin >> N >> M;
+    
+    bool arr[N+1] = {false, };
+    vector<int> v[N+1];
+
+    for(int i = 0; i < M; i++){
+        int a, b;
+        cin >> a >> b;
+    }
+
+    
+    cout << N -1 << endl;
+
+    
+}
+
+int main(){
+    int T;
+    cin >> T;
+    
+    while(T--){
+        solve();
+    }
+    
+
+    return 0;
+}
