b0ded86116d77e0145cbfbf4b48e5fa6240abd5c
eun_jelly <dldmswp3@naver.com>
2024-01-23 00:27:51 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    string s;
+    
+    cin >> s;
+    cout << s.size() << endl;
+
+
+    return 0;
+}
