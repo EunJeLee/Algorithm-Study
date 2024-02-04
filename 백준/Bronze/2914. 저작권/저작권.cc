e501637ae84c124fe617cf9189d8ed3cf431f40e
eun_jelly <dldmswp3@naver.com>
2024-02-04 18:32:10 +0900

    [Bronze III] Title: 저작권, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <cmath>
+#define endl "\n"
+#define MOD 10007
+#define lli long long
+#define mp make_pair
+#define pb push_back
+
+using namespace std;
+
+vector<float> v;
+
+void solve(){
+  
+}
+
+int main(){
+    float a, b, x;
+
+    cin >> a >> b;
+
+
+    cout << (b-1)*a +1;
+}
+  
