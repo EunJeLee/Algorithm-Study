35e9bc244e70f8d53533207e6b1f1db2d00ee0a7
eun_jelly <dldmswp3@naver.com>
2024-02-04 15:51:56 +0900

    [Bronze V] Title: A+B - 5, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string>
+#define endl "\n"
+#define MOD 10007
+#define lli long long
+#define mp make_pair
+#define pb push_back
+
+using namespace std;
+
+int main(){
+
+  while(true){
+    int a, b;
+
+    cin >> a >> b;
+    if(a + b == 0) break;
+    cout << a + b << endl;
+  }
+}
