f64374d7384fcd2933ed48531dd3035b478894e5
eun_jelly <dldmswp3@naver.com>
2024-02-04 18:00:22 +0900

    [Bronze III] Title: 할로윈의 사탕, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <cmath>
+#define endl "\n"
+#define MOD 10007
+#define lli long long
+#define mp make_pair
+#define pb push_back
+
+using namespace std;
+
+vector<float> v;
+
+void solve(){
+  
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        int a, b;
+
+        cin >> a >> b;
+
+        cout << "You get " << a/b << " piece(s) and your dad gets " << a%b << " piece(s)." << endl;
+    }
+}
+  
