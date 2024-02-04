482d18b35a16dc4b812a70ad095620581ac117f2
eun_jelly <dldmswp3@naver.com>
2024-02-04 15:50:54 +0900

    [Bronze V] Title: A+B - 3, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string>
+#define endl "\n"
+#define MOD 10007
+#define lli long long
+#define mp make_pair
+#define pb push_back
+
+using namespace std;
+
+int main(){
+
+  int n;
+  cin >> n;
+
+  for(int i = 0; i < n; i++){
+    int a, b;
+    cin >> a >> b;
+    cout << a+b << endl;
+  }
+}
