fe4baa2b40bb8a16bcf33fc858ba7110b63ab606
eun_jelly <dldmswp3@naver.com>
2024-02-04 15:46:38 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string>
+#define endl "\n"
+#define MOD 10007
+#define lli long long
+#define mp make_pair
+#define pb push_back
+
+using namespace std;
+
+int main(){
+
+   int n;
+   cin >> n;
+   for(int i = 1; i <= n; i++) cout << i << endl;
+}
