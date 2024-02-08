2d4d6eb8fc955f52e5d41c2f526554b8d2c68099
eun_jelly <dldmswp3@naver.com>
2024-02-08 23:44:09 +0900

    [Bronze III] Title: 다면체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#define endl "\n"
+#define MOD 10007
+#define lli long long
+#define mp make_pair
+#define pb push_back
+
+using namespace std;
+
+void solve(){
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << b - a + 2 << endl;
+}
+
+bool compare(int a, int b){
+    return a > b;
+}
+
+
+
+int main(){
+
+    int t;
+    cin >> t;
+    while(t--)solve();
+
+}
+  
