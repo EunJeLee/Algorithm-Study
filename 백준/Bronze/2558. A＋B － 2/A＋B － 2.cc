a4a34e3954299df77c66d5629d8f9ffeac584b3d
eun_jelly <dldmswp3@naver.com>
2024-02-09 16:01:53 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2304 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <cstring>
+#define endl "\n"
+#define MOD 10007
+#define lli long long
+#define mp make_pair
+#define pb push_back
+
+using namespace std;
+
+bool compare(const pair<int,int>a, const pair<int, int>b){
+    if(a.second == b.second) return a.first < b.first;
+    return a.second > b.second;
+}
+
+vector<int> v[10005];
+vector<pair<int, int> > ans;
+int d[10005];
+bool visited[10005];
+int n, m;
+int cnt = 1;
+
+void dfs(int x){
+    visited[x] = true;
+
+    for(int i = 0; i < v[x].size(); i++){
+        if(!visited[v[x][i]]) {
+            visited[v[x][i]] = true;
+            cnt++;
+            dfs(v[x][i]);
+        }
+        
+    }
+}
+
+
+
+int main(){
+    
+    lli a, b;
+
+    cin >> a >> b;
+
+    cout << a+b << endl;
+ }
+
