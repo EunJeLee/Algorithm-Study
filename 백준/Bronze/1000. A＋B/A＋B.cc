36fd8a4aa5c67dd7137e15d42b4f25ded3e9ee6d
eun_jelly <dldmswp3@naver.com>
2024-02-19 23:04:53 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 9912 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <list>
+#include <map>
+#include <deque>
+#define endl "\n"
+#define MOD 10007
+#define mp make_pair
+#define pb push_back
+
+using namespace std;
+
+int lcs[1005][1005];
+stack<char> s;
+int dp[1005][1005];
+
+
+int main(){
+    int a, b;
+
+    cin >> a;
+
+    cin >> b;
+
+    cout << a + b;
+
+    
+    
+
+    return 0;
+}
