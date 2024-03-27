6b1d217f2fe543fc4e9446ab5769ad4407e80f09
eun_jelly <dldmswp3@naver.com>
2024-03-28 01:46:31 +0900

    [Silver V] Title: 날짜 계산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <list>
+#include <map>
+#include <deque>
+#include <tuple>
+#define endl "\n"
+#define MOD 10007
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define lli long long
+#define FASTIO() ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+using namespace std;
+
+int dy[4] = {0, 1, 0, -1};
+int dx[4] = {-1, 0, 1, 0};
+int n, m;
+string b = "BWBWBWBW";
+string w = "WBWBWBWB";
+
+string case1[8] = {b, w, b, w, b, w, b, w};
+string case2[8] = {w, b, w, b, w, b, w, b};
+vector<string> v;
+
+
+struct compare {
+    bool operator()(const pair<lli, lli> a, const pair<lli, lli> b) {
+       if(a.first == b.first) return a.second < b.second;
+       return a.first > b.first;
+    }
+};
+
+
+void input(){
+
+}
+
+void solve(){
+  
+}
+
+int main() {
+
+   int a, b, c;
+   cin >> a>> b>>c;    
+   int year = 1;
+    while(year){
+      if((year-a) % 15 == 0 && (year-b) % 28 == 0 && (year-c) % 19 == 0){
+         cout << year <<endl;
+         break;
+      }
+      year++;
+    }
+ 
+    
+}   
